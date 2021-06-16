9a8b4f99b2f92238127cc55e8e55ae8632ea773a
Patel vaibhav <38983632+vaibhav0077@users.noreply.github.com>
2021-06-16 10:18:27 +0530

    Add files via upload

---
+++ Project_Email_PHP/index.php
@@ -0,0 +1,61 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "project_email_php";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+else{
+echo "Connected successfully<br>";
+}
+$sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully<br>";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
+
+$selectdb = "USE $dbname";
+if ($conn->query($selectdb) === TRUE) {
+    echo "Database selected successfully<br>";
+  } else {
+    echo "Error creating database: " . $conn->error;
+  }
+
+$subscription = "CREATE TABLE IF NOT EXISTS subscription (
+Email VARCHAR(255),
+Password VARCHAR(255),
+Status VARCHAR(255)
+)";
+
+if ($conn->query($subscription) === TRUE) {
+    echo "Table subscription created successfully<br>";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$registration = "CREATE TABLE IF NOT EXISTS registration (
+Name VARCHAR(255),
+Email VARCHAR(255),
+Username VARCHAR(255),
+Password VARCHAR(255),
+Token VARCHAR(255),
+Status VARCHAR(255)
+)";
+
+if ($conn->query($registration) === TRUE) {
+    echo "Table registration created successfully<br>";
+	echo '<script>alert("All Set ! Now You Can Run The Project ...")</script>';
+	echo '<script> window.location.href="Registration.php"</script>';
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+$conn->close();
+?>
